b4e130ed8eea8664b925d902d3835f0493cc4c13
faiq <faiqrazarizvi@gmail.com>
2014-09-15 23:02:18 -0400

    yo

--- time.c
+++ time.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h> 
+#include "time.h"
+
+inline unsigned long long rdtsc() {
+  unsigned int lo, hi;
+  asm volatile (
+     "cpuid \n"
+     "rdtsc" 
+   : "=a"(lo), "=d"(hi) /* outputs */
+   :                    /* inputs */
+   : "%ebx", "%ecx");     /* clobbers*/
+  return (((unsigned long long)hi) << 32) | lo;
+}
+
+void simple_time(){ 
+  unsigned long long start = rdtsc(); 
+  int pid = getpid(); 
+  unsigned long long stop = rdtsc(); 
+  unsigned long long cycles = start - stop;
+  printf("The process id is %d\n", pid);
+  printf("The time it took to execute is %llu\n", cycles/3591338000);
+}
 
 int main (int agrc, char ** argv){ 
-
+  int i;
+  for (i =0; i < 10; i++) simple_time(); 
 } 
---
+++ time.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE 
+inline unsigned long long rdtsc();
+void simple_time();
+#endif
